8364683b464e00536fabb1b61c1a23f3f0b9f1d4
Unknown <csmidge2@hotmail.co.uk>
2019-11-02 14:25:02 +0000

    Added voxelmap
    Unoptimised mess

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,11 +4,120 @@ using UnityEngine;
 
 public class MeshGenerator
 {
-   public void GenerateChunkMesh(ChunkData _chunk)
+   public MeshData GenerateChunkMesh(ChunkData _chunk)
     {
-        for (int x = 0; x < _chunk.height; x++)
+        MeshData meshData = new MeshData();
+
+        for (int x = 0; x < _chunk.width; x++)
         {
-          
+            for (int y = 0; y < _chunk.height; y++)
+            {
+                for (int z = 0; z < _chunk.width; z++)
+                {
+                    if (_chunk[x, y, z] == 0.0f)
+                        continue;
+
+                    if (x + 1 >= _chunk.width || _chunk[x+1,y,z] == 0)
+                        meshData.AddSquare(new Vector3(x, y, z), new Vector3(1,0,0));
+                    if (x - 1 < 0 || _chunk[x - 1, y, z] == 0)
+                        meshData.AddSquare(new Vector3(x, y, z), new Vector3(-1, 0, 0));
+
+                    if (y + 1 >= _chunk.height || _chunk[x, y + 1, z] == 0)
+                        meshData.AddSquare(new Vector3(x, y, z), new Vector3(0, 1, 0));
+                    if (y - 1 < 0 || _chunk[x, y - 1, z] == 0)
+                        meshData.AddSquare(new Vector3(x, y, z), new Vector3(0, -1, 0));
+
+                    if (z + 1 >= _chunk.width || _chunk[x, y, z + 1] == 0)
+                        meshData.AddSquare(new Vector3(x, y, z), new Vector3(0, 0, 1));
+                    if (z - 1 < 0 || _chunk[x, y, z - 1] == 0)
+                        meshData.AddSquare(new Vector3(x, y, z), new Vector3(0, 0, -1));
+                }               
+            }
         }
+        return meshData;
+    }
+}
+
+public class MeshData
+{
+    public List<Vector3> vertices;
+    public List<int> triangles;
+    public List<Vector2> uvs;
+    public List<Vector3> normals;
+
+    int verticesIndex;
+
+    public MeshData()
+    {
+        vertices = new List<Vector3>();
+        uvs = new List<Vector2>();
+        triangles = new List<int>();
+        normals = new List<Vector3>();
+    }
+
+    public void AddSquare(Vector3 _inner, Vector3 _direction)
+    {
+        if (Mathf.Abs(_direction.x) == 1.0f)
+        {
+            float x = _inner.x + _direction.x * 0.5f;
+            vertices.Add(new Vector3(x, _inner.y + 0.5f, _inner.z + 0.5f));
+            vertices.Add(new Vector3(x, _inner.y - 0.5f, _inner.z + 0.5f));
+            vertices.Add(new Vector3(x, _inner.y + 0.5f, _inner.z - 0.5f));
+            vertices.Add(new Vector3(x, _inner.y - 0.5f, _inner.z - 0.5f));
+        }
+        else if (Mathf.Abs(_direction.y) == 1.0f)
+        {
+            float y = _inner.y + _direction.y * 0.5f;
+            vertices.Add(new Vector3(_inner.x + 0.5f, y, _inner.z + 0.5f));
+            vertices.Add(new Vector3(_inner.x - 0.5f, y, _inner.z + 0.5f));
+            vertices.Add(new Vector3(_inner.x + 0.5f, y, _inner.z - 0.5f));
+            vertices.Add(new Vector3(_inner.x - 0.5f, y, _inner.z - 0.5f));
+        }
+        else if (Mathf.Abs(_direction.z) == 1.0f)
+        {
+            float z = _inner.z + _direction.z * 0.5f;
+            vertices.Add(new Vector3(_inner.x + 0.5f, _inner.y + 0.5f, z));
+            vertices.Add(new Vector3(_inner.x - 0.5f, _inner.y + 0.5f, z));
+            vertices.Add(new Vector3(_inner.x + 0.5f, _inner.y - 0.5f, z));
+            vertices.Add(new Vector3(_inner.x - 0.5f, _inner.y - 0.5f, z));
+        }
+
+        normals.Add(_direction);
+        normals.Add(_direction);
+        normals.Add(_direction);
+        normals.Add(_direction);
+
+        if (_direction.x - _direction.y + _direction.z > 0)
+        {
+            triangles.Add(verticesIndex);
+            triangles.Add(verticesIndex + 1);
+            triangles.Add(verticesIndex + 2);
+
+            triangles.Add(verticesIndex + 1);
+            triangles.Add(verticesIndex + 3);
+            triangles.Add(verticesIndex + 2);
+        }
+        else
+        {
+            triangles.Add(verticesIndex);
+            triangles.Add(verticesIndex + 2);
+            triangles.Add(verticesIndex + 1);
+
+            triangles.Add(verticesIndex + 3);
+            triangles.Add(verticesIndex + 1);
+            triangles.Add(verticesIndex + 2);
+        }
+
+        verticesIndex += 4;
+    }
+
+    public Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.normals = normals.ToArray();
+
+        return mesh;
     }
 }
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -7,9 +7,9 @@ public class TerrainGenerator : MonoBehaviour
     #region Public Variables
     /////////////////////////
     public bool autoUpdate = true;
-    [Range(0,32)]
+    [Range(0,64)]
     public int chunkWidth = 2;
-    [Range(0, 32)]
+    [Range(0, 255)]
     public int chunkHeight = 2;
     [Range(0, 100)]
     public int clipPercent;
@@ -23,11 +23,15 @@ public class TerrainGenerator : MonoBehaviour
     
     //Noise Offset
     public Vector3 offset;
+
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
     /////////////////////////
     #endregion
 
     //The noisemap used to generate terrain
     private NoiseMap3D noiseMap;
+    private MeshGenerator meshGenerator;
 
     //Used for lerping
     float maxNoise;
@@ -39,6 +43,7 @@ public class TerrainGenerator : MonoBehaviour
     public void GenerateMap()
     {
         terrainData = new ChunkData(chunkWidth,chunkHeight);
+        meshGenerator = new MeshGenerator();
         minNoise = float.MaxValue;
         maxNoise = float.MinValue;
 
@@ -58,6 +63,14 @@ public class TerrainGenerator : MonoBehaviour
             }
         }
 
+        for (int j = 0; j < chunkWidth * chunkHeight * chunkWidth; j++)
+        {
+            if (terrainData.terrainMap[j] / maxNoise * 100 <= clipPercent)
+                terrainData.terrainMap[j] = 0;
+        }
+
+        var meshData = meshGenerator.GenerateChunkMesh(terrainData);
+        meshFilter.sharedMesh = meshData.CreateMesh();
     }
 
     /// <summary>
@@ -65,22 +78,22 @@ public class TerrainGenerator : MonoBehaviour
     /// </summary>
     private void OnDrawGizmos()
     {
-        if (terrainData != null)
-        {
-            int i = 0;
-            for (int z = 0; z < chunkWidth; z++)
-            {
-                for (int y = 0; y < chunkHeight; y++)
-                {
-                    for (int x = 0; x < chunkWidth; x++)
-                    {                    
-                        Gizmos.color = Color.Lerp(Color.white, Color.black, Mathf.InverseLerp(minNoise,maxNoise, terrainData.terrainMap[i]));
-                        if (terrainData.terrainMap[i] / maxNoise * 100 > clipPercent)
-                            Gizmos.DrawSphere(new Vector3(x, y, z),Mathf.InverseLerp(((float)clipPercent)/100.0f,maxNoise, terrainData.terrainMap[i]) /2 );
-                        i++;
-                    }
-                }
-            }
-        }
+    //    if (terrainData != null)
+    //    {
+    //        int i = 0;
+    //        for (int z = 0; z < chunkWidth; z++)
+    //        {
+    //            for (int y = 0; y < chunkHeight; y++)
+    //            {
+    //                for (int x = 0; x < chunkWidth; x++)
+    //                {                    
+    //                    Gizmos.color = Color.Lerp(Color.white, Color.black, Mathf.InverseLerp(minNoise,maxNoise, terrainData.terrainMap[i]));
+    //                    if (terrainData.terrainMap[i] / maxNoise * 100 > clipPercent)
+    //                        Gizmos.DrawSphere(transform.position + new Vector3(x, y, z),Mathf.InverseLerp(((float)clipPercent)/100.0f,maxNoise, terrainData.terrainMap[i]) /2 );
+    //                    i++;
+    //                }
+    //            }
+    //        }
+    //    }
     }
 }
